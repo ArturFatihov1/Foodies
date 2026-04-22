079e6b25e6510dfe7f822eec29670c716a5b2179
Artur <artur.fatihov23@mail.ru>
2026-04-22 10:37:06 +0500

    update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,14 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.androidx.room)
+    alias(libs.plugins.ksp)
+}
+
+room {
+    schemaDirectory("debug", "$projectDir/schemas/debug")
+    schemaDirectory("release", "$projectDir/schemas/release")
 }
 
 android {
@@ -40,36 +48,40 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.compose.runtime.livedata)
-    implementation(libs.retrofit)
-    implementation(libs.converter.gson)
-    implementation(libs.coil.compose)
-
+    /** Core **/
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.activity.compose)
+    /** Compose **/
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.androidx.material3.icons.extended)
     implementation(libs.androidx.navigation.compose)
 
-
+    implementation(libs.androidx.lifecycle.viewmodel)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.lifecycle.runtime.compose)
+    implementation(libs.kotlin.serialization.json)
+    /** DI **/
     implementation(platform(libs.koin.bom))
-
-    // Add dependencies without versions
     implementation(libs.koin.android)
     implementation(libs.koin.compose)
-
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
-    implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.compose.foundation.layout)
-    implementation(libs.play.services.analytics.impl)
+    /** Coil **/
+    implementation(libs.coil.compose)
+    /** Retrofit **/
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+    /** Room **/
+    implementation(libs.room.ktx)
+    ksp(libs.room.compiler)
+    /** Test **/
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
 }
