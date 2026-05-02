fc03d063f1cbcda115bbc7e5f652921f30c6d844
Artur <artur.fatihov23@mail.ru>
2026-05-03 01:25:24 +0500

    add cart usecases

---
+++ app/src/main/java/com/example/foodies/domain/feature/cart/IncrementCartUseCase.kt
@@ -0,0 +1,7 @@
+package com.example.foodies.domain.feature.cart
+
+import com.example.foodies.domain.CartRepository
+
+class IncrementCartUseCase(private val repository: CartRepository) {
+    suspend fun execute(id: Long) = repository.increment(id)
+}
